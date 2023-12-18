da5d3e077a85d0627032ee366855b588a7c3a3f1
Umeshthoriya <154047088+Umeshthoriya@users.noreply.github.com>
2023-12-19 02:31:41 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,11 +1,13 @@
-# Take n value
-n = 12
+num = int(input("Enter a number: "))
 
-# Take an empty list to store the generated sequence
-output = []
+factorial = 1
 
-# Generate the sequence of numbers from 1 to n
-for i in range(1, n+1):
-    output.append(i)
-
-print(output)
+# check if the number is negative, positive or zero
+if num < 0:
+   print("Sorry, factorial does not exist for negative numbers")
+elif num == 0:
+   print("The factorial of 0 is 1")
+else:
+   for i in range(1,num + 1):
+       factorial = factorial*i
+   print("The factorial of",num,"is",factorial)
