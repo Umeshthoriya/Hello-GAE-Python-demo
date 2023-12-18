56cc033ab32dccd82ca30327822bbe644b2a21a5
Umeshthoriya <154047088+Umeshthoriya@users.noreply.github.com>
2023-12-19 02:16:13 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,11 @@
-print('Hello GAE')
+# Take n value
+n = 12
+
+# Take an empty list to store the generated sequence
+output = []
+
+# Generate the sequence of numbers from 1 to n
+for i in range(1, n+1):
+    output.append(i)
+
+print(output)
